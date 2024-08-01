cb540f1c604da21a1c7422f4e392172b9895ec44
Gaganvaishnav <107857658+gaganvaishnav111@users.noreply.github.com>
2024-08-01 09:33:24 +0530

    Delete quiz directory

--- quiz/src/main/java/org/quizapplication/quiz/dao/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.quizapplication.quiz.dao;
-
-import org.quizapplication.quiz.models.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer,Integer> {
-}
